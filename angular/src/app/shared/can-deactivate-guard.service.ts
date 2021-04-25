7e1173e020a9f34dc837ce53f99bb160dc526e53
Anton Shevchuk <w.s.f.antonshevchuk@gmail.com>
2021-04-25 22:24:31 +0300

    angular upgrade to 9

--- angular/src/app/shared/can-deactivate-guard.service.ts
+++ angular/src/app/shared/can-deactivate-guard.service.ts
@@ -4,11 +4,13 @@ import {
     ActivatedRouteSnapshot,
     RouterStateSnapshot
 } from '@angular/router';
+import { Injectable } from "@angular/core";
 
 export interface CanComponentDeactivate {
     canDeactivate: () => Observable<boolean> | Promise<boolean> | boolean;
 }
 
+@Injectable()
 export class CanDeactivateGuard
     implements CanDeactivate<CanComponentDeactivate> {
     canDeactivate(
