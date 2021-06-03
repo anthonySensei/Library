27aa8ecdafed3a039b2fd41731dd97523cecbcee
Anton Shevchuk <w.s.f.antonshevchuk@gmail.com>
2021-06-03 23:28:11 +0300

    refactored code

--- src/schemas/author.ts
+++ src/schemas/author.ts
@@ -2,18 +2,9 @@ import * as mongoose from 'mongoose';
 import { Schema } from 'mongoose';
 
 const authorSchema: Schema = new Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    country: {
-        type: String,
-        required: true
-    },
-    language: {
-        type: String,
-        required: true
-    }
+    name: { type: String, required: true },
+    country: { type: String, required: true },
+    language: { type: String, required: true }
 });
 
 export default mongoose.model('Author', authorSchema);
